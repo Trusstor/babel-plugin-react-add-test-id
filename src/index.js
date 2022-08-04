58cca79a188c5b1526c93e3b2eab3ef5a7504fc9
yohainnn <yohai@trusstor.com>
2022-08-04 10:40:57 +0300

    reset code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import * as t from "@babel/types";
 
 export default function(
-  api,
-  {
-    attrName = "test-id",
-    mode = "regular", // minimal, regular, full
-    ignoreElements = [],
-    additionalIgnoreElements = [],
-    delimiter = "-"
-  }
+    api,
+    {
+      attrName = "data-test-id",
+      mode = "regular", // minimal, regular, full
+      ignoreElements = [
+        "div",
+        "input",
+        "a",
+        "button",
+        "span",
+        "p",
+        "br",
+        "hr",
+        "ul",
+        "ol",
+        "li",
+        "img",
+        "form",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "svg",
+        "path",
+        "g"
+      ],
+      additionalIgnoreElements = [],
+      delimiter = "-"
+    }
 ) {
   let isRootElement = true;
-  let counter = 0;
   return {
     visitor: {
       Program(path) {
@@ -19,47 +41,51 @@ export default function(
           ClassDeclaration(path) {
             isRootElement = true;
             const componentName = path.node.id.name;
-            passDownComponentName(path, componentName, mode, delimiter, counter);
-            counter ++
+            passDownComponentName(path, componentName, mode, delimiter);
           },
           VariableDeclarator(path) {
             isRootElement = true;
             const componentName = path.node.id.name;
-            passDownComponentName(path, componentName, mode, delimiter, counter);
-            counter ++
+            passDownComponentName(path, componentName, mode, delimiter);
           },
           JSXElement(path) {
             const componentName = path.node.openingElement.name.name || "";
             const isRoot =
-              isRootElement || path.parent.type === "ReturnStatement";
+                isRootElement || path.parent.type === "ReturnStatement";
             const isIgnoredElement = [
               ...ignoreElements,
               ...additionalIgnoreElements
             ].includes(componentName);
 
             if (
-              componentName === "" ||
-              componentName.includes("Fragment") ||
-              (!isRoot && isIgnoredElement)
+                componentName === "" ||
+                componentName.includes("Fragment") ||
+                (!isRoot && isIgnoredElement)
             ) {
               return;
             }
+            // if has a key get its value
+            const keyValue = getKey(path);
 
             const concatComponentName = concatComponentsName(
-              path.node.componentName,
-              isIgnoredElement ? "" : componentName,
-              delimiter,
+                path.node.componentName,
+                isIgnoredElement ? "" : componentName,
+                delimiter,
+                keyValue
             );
 
             isRootElement = false;
 
-            const testId = t.stringLiteral(concatComponentName);
+            const testId = keyValue
+                ? t.jsxExpressionContainer(t.identifier(concatComponentName))
+                : t.stringLiteral(concatComponentName);
 
-            path.node.openingElement.attributes.push(t.jSXAttribute(attrName), testId);
+            path.node.openingElement.attributes.push(
+                t.jSXAttribute(t.jSXIdentifier(attrName), testId)
+            );
 
             mode === "full" &&
-              passDownComponentName(path, componentName, mode, delimiter, counter);
-            counter ++
+            passDownComponentName(path, componentName, mode, delimiter);
           }
         });
       }
@@ -68,41 +94,55 @@ export default function(
 }
 
 const concatComponentsName = (
-  parent = "",
-  current = "",
-  delimiter = "-",
-  counter = 0
+    parent = "",
+    current = "",
+    delimiter = "-",
+    keyValue = ""
 ) => {
-  const componentsName = parent && current ? `${parent}${delimiter}${current}${delimiter}${counter}` : parent || current;
+  const componentsName =
+      parent && current ? `${parent}${delimiter}${current}` : parent || current;
 
-  return componentsName;
+  return keyValue
+      ? `\`${componentsName}${delimiter}\${${keyValue}}\``
+      : componentsName;
 };
 
-const passDownComponentName = (path, componentName, mode, delimiter, counter = 0) => {
+const passDownComponentName = (path, componentName, mode, delimiter) => {
   let isRootElement = true;
 
   path.traverse({
     JSXElement(path) {
       if (mode === "minimal") {
         path.node.componentName =
-          isRootElement || path.parent.type === "ReturnStatement"
-            ? concatComponentsName(
-                path.node.componentName,
-                componentName,
-                delimiter,
-                counter
-              )
-            : null;
+            isRootElement || path.parent.type === "ReturnStatement"
+                ? concatComponentsName(
+                    path.node.componentName,
+                    componentName,
+                    delimiter
+                )
+                : null;
       } else {
         path.node.componentName = concatComponentsName(
-          path.node.componentName,
-          componentName,
-          delimiter,
-          counter
+            path.node.componentName,
+            componentName,
+            delimiter
         );
       }
 
       isRootElement = false;
     }
   });
+};
+
+const getKey = path => {
+  const keyAttribute = path.node.openingElement.attributes.find(
+      ({ name }) => name && name.name === "key"
+  );
+
+  const keyValue =
+      keyAttribute && keyAttribute.value && keyAttribute.value.expression
+          ? keyAttribute.value.expression.name
+          : "";
+
+  return keyValue;
 };
